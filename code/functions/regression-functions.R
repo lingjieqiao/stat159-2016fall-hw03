fedd8ebbb64c44b31adb7d66567abb0a4292f210
Lingjie Qiao <katherine_qiao@berkeley.edu>
2016-10-14 13:59:23 -0700

    Add and Modify README files

---
+++ code/functions/regression-functions.R
@@ -0,0 +1,34 @@
+# Import data
+advertising <- read.csv("../../data/Advertising.csv", header = TRUE)
+
+# Fit multiple linear regression model
+lm_model <- lm(Sales ~ TV + Radio + Newspaper, data = advertising)
+
+# Functions
+# Residual Sum of Squares: this function takes in the lm object, and output Residual Sum of Squares
+residual_sum_squares <- function(lm_model) {
+    summary <- summary(lm_model)
+    sum(summary$residuals^2)
+}
+
+# Total Sum of Squares: this function takes in the lm object, and output Total Sum of Squares
+total_sum_squares <- function(lm_model) {
+    rss <- residual_sum_squares(lm_model)
+    summary <- summary(lm_model)
+    rss / (1 - summary$r.squared)
+}
+
+# R Squared: this function takes in the lm object, and output R squared
+r_squared <- function(lm_model) {
+    
+}
+
+# F statistics: this function takes in the lm object, and output F statistics
+f_statistic <- function(lm_model) {
+    
+}
+
+# Residual Standard Error: this function takes in the lm object, and output Residual Standard Error
+residual_std_error <- function(lm_model) {
+    
+}
